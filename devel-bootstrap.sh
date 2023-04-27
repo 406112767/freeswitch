08de398e4b524b28f9dc9b9b60ef0c78374a9052
廖新刚 <liaoxingang@jiayinfintech.cn>
2023-04-27 13:59:23 +0800

    [Feature] 增加编译脚本注释

--- devel-bootstrap.sh
+++ devel-bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 使用以下配置可打印读写锁日志
+# export CFLAGS="-ggdb3 -O0 -DSWITCH_DEBUG_RWLOCKS"
+# export CXXFLAGS="-ggdb3 -O0 -DSWITCH_DEBUG_RWLOCKS"
 export CFLAGS="-ggdb3 -O0"
 export CXXFLAGS="-ggdb3 -O0"
 ./bootstrap.sh -j
